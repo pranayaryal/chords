a064f31a575dfbf815674c53d10bcf94bee8f65e
pranayaryal <drpranayaryal@gmail.com>
2018-10-06 18:12:58 -0400

    commiting after some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import Hero from './components/Hero';
-import Chord from './components/Chord'
 import Tiles from './components/Tiles';
-
 // import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = props => {
                         Ukelele Chords
                     </h1>
                     <h2 className="subtitle">
-                        Using D3 and React By Pranay
+                        Using D3 and React by Pranay
                     </h2>
                 </div>
             </div>
