48b33005e7e2ef9a80fa3bdb32cc2183eddba86b
pranayaryal <drpranayaryal@gmail.com>
2019-01-14 23:20:36 -0500

    add build folder

---
+++ build/precache-manifest.fa845421825bae4f19b3502865df72f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "781928596bbd3390dbe3",
+    "url": "/static/js/main.78192859.chunk.js"
+  },
+  {
+    "revision": "e6c262b0a85b21655f67",
+    "url": "/static/js/1.e6c262b0.chunk.js"
+  },
+  {
+    "revision": "781928596bbd3390dbe3",
+    "url": "/static/css/main.37821c4d.chunk.css"
+  },
+  {
+    "revision": "2bd47596f42743cfaa951597e3b89744",
+    "url": "/index.html"
+  }
+];
